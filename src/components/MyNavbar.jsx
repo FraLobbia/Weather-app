108a829f509be8fb221b1167b8376925924ffc72
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-01-28 20:33:16 +0100

    sistemato navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,77 +1,64 @@
 import { Nav, Container, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 function MyNavbar(props) {
-	const navBarConfig = [
-		{
-			name: "Home",
-			link: "/",
-		},
-		{
-			name: "My Location",
-			link: "/my-location",
-		},
-		{
-			name: "OpenWeather API",
-			link: "https://openweathermap.org/",
-		},
-		{
-			name: "My GitHub",
-			link: "https://github.com/FraLobbia",
-		},
-		{
-			name: "My LinkedIn",
-			link: "https://www.linkedin.com/in/francesco-lobbia/",
-		},
-	];
-
 	return (
 		<Navbar expand="md" collapseOnSelect>
-			<Container fluid className="p-0">
-				<Navbar.Brand href="#">
-					<img
-						src={props.logo}
-						className="App-logo ms-4"
-						alt="logo"
-						style={{ height: "150px" }}
-					/>
-				</Navbar.Brand>
-				<Navbar.Toggle
-					className="ms-auto me-5"
-					aria-controls="myNavbar"
-				/>
+			<Container
+				fluid
+				className="flex-column flex-md-row align-items-center">
+				<LinkContainer to={"/"}>
+					<Navbar.Brand>
+						<img
+							src={props.logo}
+							className="App-logo mb-3"
+							alt="logo"
+							style={{ height: "150px" }}
+						/>
+					</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle aria-controls="myNavbar" />
 
 				<Navbar.Collapse id="myNavbar" className="text-center">
 					<hr className="d-md-none" />
 
 					<Nav className=" align-items-center ">
-						{navBarConfig.map((navItem, index) =>
-							index === 2 ? (
-								<LinkContainer
-									to={navItem.link}
-									key={`navItem-${index}`}>
-									<Nav.Link
-										className="nav-link mx-1"
-										target={index > 1 ? "_blank" : ""}>
-										<div className="vr mx-3"></div>
-										{navItem.name}
-									</Nav.Link>
-								</LinkContainer>
-							) : (
-								<LinkContainer
-									to={navItem.link}
-									key={`navItem-${index}`}>
-									<Nav.Link
-										className={
-											index < 2
-												? "fs-3 nav-link mx-1"
-												: "nav-link mx-1"
-										}
-										target={index > 1 ? "_blank" : ""}>
-										{navItem.name}
-									</Nav.Link>
-								</LinkContainer>
-							)
-						)}
+						<LinkContainer to={"/"}>
+							<Nav.Link className="nav-link mx-1 fs-1">
+								Home
+							</Nav.Link>
+						</LinkContainer>
+
+						<LinkContainer to={"my-location"}>
+							<Nav.Link className={"nav-link mx-1 fs-1"}>
+								My Location
+							</Nav.Link>
+						</LinkContainer>
+
+						<div className="vr mx-3 d-none d-md-block"></div>
+
+						<Nav.Link
+							href="https://openweathermap.org/"
+							className="nav-link mx-1"
+							target="_blank" // Apri il link in una nuova finestra
+						>
+							OpenWeather API
+						</Nav.Link>
+
+						<Nav.Link
+							href="https://github.com/FraLobbia"
+							className="nav-link mx-1"
+							target="_blank" // Apri il link in una nuova finestra
+						>
+							My GitHub
+						</Nav.Link>
+
+						<Nav.Link
+							href="https://www.linkedin.com/in/francesco-lobbia/"
+							className="nav-link mx-1"
+							target="_blank" // Apri il link in una nuova finestra
+						>
+							My LinkedIn
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
