4a9591d2e5268e005fce8a28bb9f8f0498c0def0
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-01-28 12:10:43 +0100

    test

--- src/assets/token.js
+++
@@ -1,1 +0,0 @@
-export const token = "a1f4e19dc76d4cef471044722e1ee296";
--- src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h2 className="fs-1 fw-bold">Benvenuto!</h2>
-			<p className="fs-3">Per cominciare cerca la tua città!</p>
-		</div>
-	);
-};
-export default Home;
